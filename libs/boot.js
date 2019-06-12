c631affc9802b035f694df9ef51f5f14ad643c51
Medo-Work <medo@bibimapp.com>
2019-06-12 15:12:28 +0200

    fixed books seeder

--- libs/boot.js
+++ libs/boot.js
@@ -1,6 +1,6 @@
 module.exports = app => {
     if (process.env.NODE_ENV !== "test") {
-        app.db.sequelize.sync().done(() => {
+        app.db.sequelize.sync({force: true}).done(() => {
             app.listen(app.get("port"), () => {
                 console.log(`Bibliotech API - Port ${app.get("port")}`);
             });
--- seeders/20190612075033-demo-books-institution.js
+++ seeders/20190612075033-demo-books-institution.js
@@ -16,14 +16,14 @@ module.exports = {
         let bulkbooks = [];
         // Generate a Unique set of Bookid and InstitutionId to avoid DB validation error
         let unqiuebooks = new Set();
-        let bookId = Math.floor(Math.random() * Math.floor(25));
         const InstitutionCount = [1, 2, 3, 4, 5];
         for(let ins of InstitutionCount) {
             [...Array(3)].map((_, i) => {
-                let bookId = Math.floor(Math.random() * Math.floor(25));
+                let bookId = Math.floor(Math.random() * Math.floor(19));
                 unqiuebooks.add([ins, bookId === 0? bookId + 1 : bookId])
             })
         }
+        unqiuebooks = multiDimensionalUnique([...unqiuebooks])
         for(let i of unqiuebooks) {
             let book =  {
                 InstitutionId: i[0],
@@ -35,7 +35,7 @@ module.exports = {
         }
 
         console.log(bulkbooks);
-        return queryInterface.bulkInsert('InstitutionBooks', multiDimensionalUnique(bulkbooks), {});
+        return queryInterface.bulkInsert('InstitutionBooks', bulkbooks, {});
     },
 
     down: (queryInterface, Sequelize) => {
