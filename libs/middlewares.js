0881a20510120a4f712cce7d04d0401a58efe137
Medo-Work <medo@bibimapp.com>
2019-06-12 15:24:49 +0200

    improving the app security and performance

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,8 +1,17 @@
 import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
 
 module.exports = app => {
     app.set("port", app.libs.config.port);
     app.set("json spaces", 4);
+    app.use(helmet ());
+    app.use(cors({
+        //we limit allowed domain, I'm putting 3001 a fake port assuming we have a frontend app for example
+        origin: ["http://localhost:3001"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    }));
     app.use(bodyParser.json());
     app.use(app.auth.initialize());
 };
